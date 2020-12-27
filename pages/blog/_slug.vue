767e6a2030df267c6c57da623561584fc6345c47
Gek Siong Low <geksiong@yahoo.com>
2020-12-27 16:31:36 +0800

    feat: format post date

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="prose lg:prose-xl">
     <h1>{{ article.title }}</h1>
-    <p>{{ article.date }}</p>
+    <p>Updated {{ formatDate(article.date) }}</p>
     <nuxt-content :document="article" />
   </article>
 </template>
@@ -12,6 +12,12 @@ export default {
     const article = await $content('blog', params.slug).fetch()
 
     return { article }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'short', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    },
   },
 }
 </script>
